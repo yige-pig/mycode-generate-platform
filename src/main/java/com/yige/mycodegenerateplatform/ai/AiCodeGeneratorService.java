93b73ab4cc391a626dfaafaaa2fb91bf65a31a11
Luoyi Lin <linluoyi666@outlook.com>
2026-04-15 19:18:42 +0800

    feat：实现流式输出

--- src/main/java/com/yige/mycodegenerateplatform/ai/AiCodeGeneratorService.java
+++ src/main/java/com/yige/mycodegenerateplatform/ai/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.yige.mycodegenerateplatform.ai;
 import com.yige.mycodegenerateplatform.ai.model.HtmlCodeResult;
 import com.yige.mycodegenerateplatform.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
 
@@ -23,6 +24,25 @@ public interface AiCodeGeneratorService {
      */
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
     MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+    /**
+     * 生成 HTML 代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
+
 
 }
 
