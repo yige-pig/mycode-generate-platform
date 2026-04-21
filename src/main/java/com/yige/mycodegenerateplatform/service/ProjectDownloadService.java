7e581ad26175d9a997cc8e431ae868f011e84db7
Luoyi Lin <linluoyi666@outlook.com>
2026-04-21 20:33:20 +0800

    feat: 开发下载代码功能

---
+++ src/main/java/com/yige/mycodegenerateplatform/service/ProjectDownloadService.java
@@ -0,0 +1,7 @@
+package com.yige.mycodegenerateplatform.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
