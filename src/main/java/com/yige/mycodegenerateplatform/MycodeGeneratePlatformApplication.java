5698170f194bd65c22f1236f40e589569dd5f96e
Luoyi Lin <linluoyi666@outlook.com>
2026-04-22 14:41:03 +0800

    refactor: redis缓存优化

--- src/main/java/com/yige/mycodegenerateplatform/MycodeGeneratePlatformApplication.java
+++ src/main/java/com/yige/mycodegenerateplatform/MycodeGeneratePlatformApplication.java
@@ -4,10 +4,12 @@ package com.yige.mycodegenerateplatform;
 import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableCaching
 //@MapperScan("com.yige.mycodegenerateplatform.mapper")
 public class MycodeGeneratePlatformApplication {
 
